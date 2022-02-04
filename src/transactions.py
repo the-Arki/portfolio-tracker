ff34fe0cf48fb440aca6c0ca5daa249ec2607c36
Arki <czoboly.mihaly@gmail.com>
2022-02-04 10:45:43 +0100

    separated add_transactions method in Transactins class into 2 methods (add / remove)

--- src/transactions.py
+++ src/transactions.py
@@ -6,11 +6,16 @@ You are allowed to add / modify / delete transactions in the past or present, bu
 class Transactions():
     transactions_list = []
     
-    def add_transaction(self, date, type, amount, currency):
+    def define_transaction(self, date, type, amount, currency):
         if type == "Cash-In":
             real_amount = amount
         if type == "Withdraw":
             real_amount = -amount
         transaction = {"date": date, "amount": real_amount, "currency": currency}
-        self.transactions_list.append(transaction)
         return transaction
+    
+    def add_transaction(self, transaction):
+        self.transactions_list.append(transaction)
+    
+    def remove_transaction(self, transaction):
+        self.transactions_list.remove(transaction)
