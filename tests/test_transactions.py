34dcdb70d88ec617c6206875eeb81aea59ff6438
Arki <czoboly.mihaly@gmail.com>
2022-02-05 16:13:51 +0100

    added a separator line above the first class import

--- tests/test_transactions.py
+++ tests/test_transactions.py
@@ -2,7 +2,7 @@ import os, sys
 dir_path = os.path.dirname(os.path.realpath(__file__))
 parent_dir_path = os.path.abspath(os.path.join(dir_path, os.pardir))
 sys.path.insert(0, parent_dir_path)
-
+#------------------------------------------------------
 from src.transactions import Transactions
 
 trs = Transactions()
