61d10d2b2fc502d83f723ebda4de7419cca02db9
Arki <czoboly.mihaly@gmail.com>
2022-03-10 14:36:45 +0100

    added 'create instance' and 'delete instance methods to the class. The instance names will be stored in 'files/portfolio_names.json' in 'files' directory.

--- src/portfolios.py
+++ src/portfolios.py
@@ -5,18 +5,46 @@ from cash import Cash
 from currency import Currency
 from portfolio import Portfolio
 from stock import Stock
+import io_manager
 
 
 class Portfolios:
-    portfolio_names = []
-    portfolio_data = {}
+    instances = {}
 
     def __init__(self):
-        for portfolio in self.portfolio_names:
-            name = str(portfolio)
-            portfolio = Portfolio()
-            self.portfolio_data[name] = portfolio
+        self.portfolio_names = io_manager.read_json(
+            'files/portfolio_names.json')
+        for name in self.portfolio_names:
+            self.instances[name] = Portfolio()
 
-    def get_instance(self, name):
-        instance = self.portfolio_data[name]
-        return instance
+    def create_instance(self, name):
+        self.portfolio_names.append(name)
+        self.instances[name] = Portfolio()
+        io_manager.write_json(self.portfolio_names, 'files/portfolio_names.json')
+
+    def delete_instance(self, name):
+        if name in self.portfolio_names:
+            self.portfolio_names.remove(name)
+            del self.instances[name]
+            io_manager.write_json(self.portfolio_names, 'files/portfolio_names.json')
+        else:
+            print(f'{name} is not an instance.')
+
+    def get_instance_values(self):
+        # get the sum values of instances by type (stock, bond, cash)
+        pass
+
+    def calculate_total_value(self):
+        # calculate the total value of the portfolios
+        pass
+
+###############################################################################
+lista = ['kakas', 'birka']
+io_manager.write_json(lista, 'files/portfolio_names.json')
+x = Portfolios()
+print(x.instances)
+x.create_instance('kukorica')
+print(x.instances)
+x.delete_instance('kakas')
+print(x.instances)
+x.delete_instance('kakas')
