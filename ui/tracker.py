d814081baab777cd7de6c4492c033aa84fc88714
Czoboly Mihaly <czoboly.mihaly@gmail.com>
2022-04-26 23:37:38 +0200

    removed MyDialog class from main class and renamed the kv file.

--- ui/tracker.py
+++ ui/tracker.py
@@ -2,8 +2,8 @@ import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
-from kivy.lang import Builder
 from kivy.properties import ObjectProperty
+from kivy.uix.boxlayout import BoxLayout
 from kivymd.app import MDApp
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.button import MDFlatButton
@@ -14,59 +14,58 @@ from kivymd.uix.toolbar import MDToolbar
 from src.main import Portfolios
 
 portfolios = Portfolios()
-main_kv_file = "main.kv"
+main_kv_file = "mainscreen.kv"
 
 
-class AddPortfolio(MDBoxLayout):
-    name = ObjectProperty(None)
+class Main(MDApp):
 
-    def get_name(self):
-        print(self.name)
-
-class Tracker(MDApp):
-    dialog = None
     def build(self):
         self.load_kv(main_kv_file)
         return self.root
 
-    # it has to be deleted (in .kv file as well)
-    def test(self):
-        print('button has been pushed')
+    def call_method(self, klass_, method_):
+        klass = getattr(sys.modules[__name__], klass_)
+        x = klass()
+        method = getattr(x, method_)
+        method()
 
-    def add_portfolio(self):
-        new = AddPortfolio()
-        self.dialog = MDDialog(
-            title="Add new portfolio",
-            type="custom",
-            content_cls=new,
-            buttons=[
+class AddPortfolio(MDBoxLayout):
+    name = ObjectProperty()
+    pass
+
+
+class MyDialog(MDDialog):
+
+    def __init__(self):
+        kwargs = {
+            'content_cls': AddPortfolio(),
+            'title': 'Add new portfolio',
+            'type': 'custom',
+            'buttons': [
                 MDFlatButton(
                     text="Cancel",
-                    on_release=self.close_dialog),
+                    on_release=lambda _: self.dismiss()
+                ),
                 MDFlatButton(
-                    text="Add",
-                    on_release=self.get_text
-                    )
+                    text='Add',
+                    on_release=lambda _: self.get_text()
+                )
             ]
-            )
-        self.dialog.open()
+        }
+        super().__init__(**kwargs)
 
-    def get_text(self, inst):
-        textfield = self.dialog.content_cls.name
+    def get_text(self):
+        textfield = self.content_cls.name
         name = textfield.text
         if name:
-            print(self.dialog.content_cls.name.text)
+            print(self.content_cls.name.text)
             if name in portfolios.instances.keys():
                 textfield.error=True
                 textfield.helper_text="{} has already been created".format(name)
             else:
                 portfolios.create_instance(name)
-                self.close_dialog(inst)
-
-    def close_dialog(self, inst):
-        self.dialog.dismiss()
-        self.dialog = None
+                self.dismiss()
 
 
 if __name__ == "__main__":
-    Tracker().run()
+    Main().run()
