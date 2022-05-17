eed07936876d720d29383d315003a11e9f6a2ee9
Czoboly Mihaly <czoboly.mihaly@gmail.com>
2022-05-17 12:04:52 +0200

    added test cases to test the variables in Equity class __init__ method

--- tests/test_equity.py
+++ tests/test_equity.py
@@ -12,6 +12,21 @@ def test__get_info(mocker):
     mocker.patch('src.stock.Equity._get_info', return_value=equity_info)
     assert equity._get_info() == equity_info
 
+def test_instance_info(mocker):
+    mocker.patch('src.stock.Equity._get_info', return_value=equity_info)
+    equity = Equity('MSFT')
+    assert equity.info == equity_info
+
+def test_instance_currency(mocker):
+    mocker.patch('src.stock.Equity._get_info', return_value=equity_info)
+    equity = Equity('MSFT')
+    assert equity.currency == equity_info['currency']
+
+def test_instance_tradeable(mocker):
+    mocker.patch('src.stock.Equity._get_info', return_value=equity_info)
+    equity = Equity('MSFT')
+    assert equity.tradeable == equity_info['tradeable']
+   
 def test_tradeable():
     assert equity.is_tradeable() == equity_info['tradeable']
 
